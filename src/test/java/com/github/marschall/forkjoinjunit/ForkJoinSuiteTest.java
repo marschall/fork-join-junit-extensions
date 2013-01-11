5580e786478a332e7ea5bbe4cc6e32b243c2c62a
Philippe Marschall <pm@netcetera.ch>
2013-01-11 17:14:37 +0100

    test for simpler configuration

--- src/test/java/com/github/marschall/forkjoinjunit/ForkJoinSuiteTest.java
+++ src/test/java/com/github/marschall/forkjoinjunit/ForkJoinSuiteTest.java
@@ -1,17 +1,12 @@
 package com.github.marschall.forkjoinjunit;
 
 import org.junit.Test;
-import org.junit.internal.builders.JUnit4Builder;
 import org.junit.runner.RunWith;
-
-import com.github.marschall.forkjoinjunit.ForkJoinParameters;
-import com.github.marschall.forkjoinjunit.ForkJoinSuite;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(ForkJoinSuite.class)
-@ForkJoinParameters(runnerBuilder = JUnit4Builder.class, parallelism = 2)
 public class ForkJoinSuiteTest {
 
   @Test
